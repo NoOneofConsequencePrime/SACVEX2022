0e513627337feffb1fe79abc157f2888ae486ec9
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2022-11-30 15:34:50 -0500

    Increased rotation precision
    - Gets rotation/time derivative
    - Checks if "flat enough"
    - Organization of OOP

--- AndyJr.TheFourth/include/Auton.h
+++ AndyJr.TheFourth/include/Auton.h
@@ -1,14 +1,20 @@
 #ifndef AUTON_H
 #define AUTON_H
 
+#include <vex.h>
+
 class Auton {
   private:
+    double WasteDelay;
+    double lastDeg, lastTime;// msec
     double getAvgMotorError(double LFTarget, double LBTarget, double RFTarget, double RBTarget);
     double getRotError(double initRot, double targetRot);
+    double getRotOverTime();// (d)deg/(d)msec;
 
   public:
+    Auton(double WasteDelay);
     void moveFwdPID(double dist, double spd, double distErrorMargin, double distKP, double dirErrorMargin, double dirKP, double moveDelay);
-    void turn(double targetRot, double spd, double errorMargin, double KP, double moveDelay);
+    void turn(double targetRot, double spd, double errorMargin, double KP, double iroc, double moveDelay);
 };
 
 #endif
